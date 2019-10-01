e21b5e054fa0bd40408f53f0149f5a09bdc114ff
yudha <yudha@yudhas-MacBook-Pro.local>
2019-10-01 14:23:37 +0700

    init project

---
+++ Core Data Siswa/SiswaTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SiswaTableViewCell.swift
+//  Core Data Siswa
+//
+//  Created by yudha on 01/10/19.
+//  Copyright © 2019 yudha. All rights reserved.
+//
+
+import UIKit
+
+class SiswaTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
